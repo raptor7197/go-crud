3afa38a3b76fd71c35e93b986fbc32ac3bfd94c9
raptor7197 <vamsikrishna75836@gmail.com>
2026-01-04 01:23:37 +0530

    feat : middlewares and logging

---
+++ handlers/json.go
@@ -0,0 +1,18 @@
+package handlers 
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func writeJSON(w http.ResponseWriter,status int,data any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(data)
+}
+
+func writeError(w http.ResponseWriter,status int,data any) {
+	writeJSON(w,status,map[string]string) {
+		"error": msg,
+	})
+}
